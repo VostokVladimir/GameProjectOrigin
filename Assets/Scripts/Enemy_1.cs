88e84fd3a6b9481ccc4b99841954e3fcea10927a
VostokVladimir <vostokirk@mail.ru>
2021-12-14 18:53:44 +0300

    Покрашены стены, Переписан скрипт для Enemy1

---
+++ Assets/Scripts/Enemy_1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_1 : MonoBehaviour
+{
+    
+    public GameObject EnemyPrefab;
+    public Transform PositionEnemy;
+
+    private void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var g06 = Instantiate(EnemyPrefab, PositionEnemy.position, Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+}
